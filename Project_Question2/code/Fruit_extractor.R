732f1c2cafee9c55e948a8ce7e9aa54f64ad676a
gabriellaneilon <126854246+gabriellaneilon@users.noreply.github.com>
2023-06-15 13:33:27 +0200

    Final

---
+++ Project_Question2/code/Fruit_extractor.R
@@ -0,0 +1,11 @@
+Fruit_extractor <- function(dff_adj, froots){
+    library(xtable)
+
+    df_sav <- dff_adj %>% group_by(country) %>%
+        summarise(Ref_froot = sum( grepl(froots, description)) / n()) %>%
+        mutate(Fruit = froots)
+
+    df_sav
+
+
+}
---
+++ Project_Question2/code/Regression_Plot.R
@@ -0,0 +1,17 @@
+Regression_Plot <- function(SA){
+
+    dfreg <- SA %>%
+        select(taster_name, points, price, province, variety)
+
+    Reglist <-
+        dfreg %>% group_by(taster_name) %>%
+        do(reg = lm(as.formula("points ~ price + province + variety"), data = .)) %>%
+        # Make each taster a list so we can use map:
+        group_split(taster_name)
+
+
+    g <- Reglist %>% map(~Reg_Graph_Creator(.)) %>% cowplot::plot_grid(plotlist = ., ncol = 1)
+
+    g
+
+}
